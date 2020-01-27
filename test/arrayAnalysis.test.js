12362a87404ab158e2f347c96973118681587fe6
ansaryergesh <yergeshovansar@gmail.com>
2020-01-27 15:03:04 +0600

    array-analyze method

---
+++ test/arrayAnalysis.test.js
@@ -0,0 +1,9 @@
+import arrayAnalysis from "../src/arrayAnalyze";
+
+test('testing arrayAnalys method',() => {
+  const array = arrayAnalysis([1,8,3,4,2,6]);
+  expect(array.average).toBe(4);
+  expect(array.min).toBe(1);
+  expect(array.max).toBe(8);
+  expect(array.length).toBe(6);
+});
