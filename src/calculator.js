54d84452cbf79d04d185a50a4d252ce9ee903711
ansaryergesh <yergeshovansar@gmail.com>
2020-01-27 15:02:03 +0600

    calculator

---
+++ src/calculator.js
@@ -0,0 +1,13 @@
+const calculator = (() => {
+  const add = (a, b) => a + b;
+    
+  const subtract = (a, b) => a - b;
+
+  const divide = (a, b) => a / b
+
+  const multiply = (a, b) => a * b;
+
+  return { add, subtract, divide, multiply, };
+})();
+
+export default calculator;
